8684114e9f99884309ad00f077c0f9d6fd70e812
Dillon <dillonmorris91@gmail.com>
2022-12-16 14:36:43 -0700

    Enlarge progress text

--- src/components/ProgressEncouragement.tsx
+++ src/components/ProgressEncouragement.tsx
@@ -18,9 +18,10 @@ export const ProgressEncouragement: React.FC<ProgressEncouragementProps> = ({
   const key = Math.floor((encouragements.length - 1) * progress)
   const keyMax = encouragements.length - 1
   const safeKey = key > keyMax ? keyMax : key
+
   return (
     <div className="flex items-center mt-6 lg:mt-7 rounded-md justify-center w-full">
-      <p className="text-sm lg:text-xl font-bold text-gray-900">
+      <p className="text-sm lg:text-2xl font-bold text-gray-900">
         {encouragements[safeKey]}
       </p>
     </div>
